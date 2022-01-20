e7c931dc051c05625ed359c1a46b177aad45a3fa
K SAI PRAVEEN <kondasaipraveen@gmail.com>
2022-01-20 13:42:17 +0530

    Assignment

--- Module_4-Solution/script.js
+++ Module_4-Solution/script.js
@@ -1,4 +1,4 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "jim"];
 for (var i = 0; i < names.length; i++) {
 	if (names[i].charAt(0) === "J" || names[i].charAt(0) === "j") {
 		console.log("Goodbye " + names[i]);
